ac4a319c65790c0870d651661de2890e85f11d38
vasilkrug <vkruglovec@gmail.com>
2023-02-15 19:52:09 +0300

    add dashboard page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import React from "react";
+import {Route, Routes,} from "react-router";
+import Dashboard from "./pages/dashboard/dashboard";
 
 const App = () => {
     return (
         <div className="App">
-            <h1>hello</h1>
+            <Routes>
+                <Route path={'/dashboard'} element={<Dashboard/>}/>
+            </Routes>
         </div>
     );
 }
