752177b6784637dd244dca30e014d3d2693c260f
vasilkrug <vkruglovec@gmail.com>
2023-02-19 17:36:32 +0300

    change CustomLineChart

--- src/components/Charts/CustomLineChart/CustomLineChart.jsx
+++ src/components/Charts/CustomLineChart/CustomLineChart.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {LineChart, CartesianGrid, Line, ResponsiveContainer, XAxis, YAxis} from "recharts";
+import {LineChart, CartesianGrid, Line, ResponsiveContainer, XAxis} from "recharts";
 
 const CustomLineChart = ({data}) => {
+
     return (
-        <ResponsiveContainer height={300}>
-            <LineChart width={600} height={300} data={data}>
-                <Line type="monotone" dataKey="age" stroke="#4318FF" strokeWidth={2} fill={'#4318FF'}/>
-                <Line type="monotone" dataKey="age2" stroke="#6AD2FF" strokeWidth={2} fill={'#6AD2FF'}/>
-                <CartesianGrid display={'none'}/>
-                <XAxis dataKey="name" stroke={'#A3AED0'}/>
-                <YAxis display={'none'}/>
-            </LineChart>
-        </ResponsiveContainer>
+            <ResponsiveContainer height={300} width={'80%'}>
+                <LineChart data={data}>
+                    <Line type="monotone" dataKey="age" stroke="#4318FF" strokeWidth={2} fill={'#4318FF'}/>
+                    <Line type="monotone" dataKey="age2" stroke="#6AD2FF" strokeWidth={2} fill={'#6AD2FF'}/>
+                    <CartesianGrid display={'none'}/>
+                    <XAxis dataKey="name" stroke={'#A3AED0'} style={{fontFamily:'DM Sans, sans-serif',fontWeight:700}}/>
+                </LineChart>
+            </ResponsiveContainer>
     );
 };
 
